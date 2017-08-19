6762ad221ba68d12a504d3215363524425497d26
sai <ram999.mail@gmail.com>
2017-08-19 14:47:28 +0530

    Update network.go

--- network.go
+++ network.go
@@ -37,7 +37,7 @@ func doEvery(d time.Duration, f func(time.Time)) {
 }
 
 func helloworld(t time.Time) {
-	fmt.Printf("%v: Hello, World!\n", t)
+	log.Println("%v: Hello, World!\n", t)
 }
 
 func decodeMessage(resp *http.Response) (msg string, err error) {
